83a117a6c0ec8f66c2e30806654a8b26ecdca2ec
JasonLev <jasond.levy@gmail.com>
2019-05-01 18:10:28 -0700

    f/init; link to Flow

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is where to learn more about Flow:
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://flow.org/en/docs/getting-started/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Flow
         </a>
       </header>
     </div>
